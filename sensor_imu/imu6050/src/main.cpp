a56380a004ee82ecee0d8348a80ccaf847c69fa9
fabian lopez <cflopezm@unal.edu.co>
2022-01-22 12:12:13 -0500

    Giroscopio funcionando con libreria wire. Datos raw del sensor

--- sensor_imu/imu6050/src/main.cpp
+++ sensor_imu/imu6050/src/main.cpp
@@ -1,87 +1,49 @@
 #include <Arduino.h>
 
-#include <Wire.h> 
-
-#define MPU 0x68
-
-// Factores de conversion
-#define A_R 16384.0
-#define G_R 131.0
-
-// conversion de rad -> deg
-#define RAD2DEG 57.295779
-
-// valores de MPU 6050 de 16 bits
-
-int16_t ax, ay, az, gx, gy, gz;
-
-// angulos
-float Acc[2];
-float Gy[3];
-float Angle[3];
-
-String values;
-long time_prev;
-float dt;
-
-
-void setup() {
-  // put your setup code here, to run once:
-
-  Wire.begin(4,5); // pin D2=SDA , D1=SCL
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  Serial.begin(115200);
-  
+// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+char tmp_str[7]; // temporary variable used in convert function
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
 }
-
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // el registro 0x3B es ax (acc x)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // se piden 6 registros desde 0x3B
-
-  ax = Wire.read()<<8|Wire.read();
-  ay = Wire.read()<<8|Wire.read();
-  az = Wire.read()<<8|Wire.read();
-
-  // A partir de los valores del acelerometro, se calculan los angulos Y, X
-  //respectivamente, con la formula de la tangente.
-  Acc[1] = atan(-1*(ax/A_R)/sqrt(pow((ay/A_R),2) + pow((az/A_R),2)))*RAD2DEG;
-  Acc[0] = atan((ay/A_R)/sqrt(pow((ax/A_R),2) + pow((az/A_R),2)))*RAD2DEG;
-
-  //Leer los valores del Giroscopio
-   Wire.beginTransmission(MPU);
-   Wire.write(0x43);
-   Wire.endTransmission(false);
-   Wire.requestFrom(MPU,6,true);   //A partir del 0x43, se piden 6 registros
-   gx = Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
-   gy = Wire.read()<<8|Wire.read();
-   gz = Wire.read()<<8|Wire.read();
- 
-   //Calculo del angulo del Giroscopio
-   Gy[0] = gx/G_R;
-   Gy[1] = gy/G_R;
-   Gy[2] = gz/G_R;
-
-   dt = (millis() - time_prev) / 1000.0;
-   time_prev = millis();
- 
-   //Aplicar el Filtro Complementario
-   Angle[0] = 0.98 *(Angle[0]+Gy[0]*dt) + 0.02*Acc[0];
-   Angle[1] = 0.98 *(Angle[1]+Gy[1]*dt) + 0.02*Acc[1];
-
-   //Integración respecto del tiempo paras calcular el YAW
-   Angle[2] = Angle[2]+Gy[2]*dt;
- 
-   //Mostrar los valores por consola
-   values = "90, " +String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]) + ", -90";
-   Serial.println(values);
-   
-   delay(10);
- 
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  //Serial.print(convert_int16_to_str(accelerometer_x));Serial.print(","); 
+  //Serial.print(convert_int16_to_str(accelerometer_y));Serial.print(","); 
+  //Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  //Serial.print(temperature/340.00+36.53);
+  Serial.print(convert_int16_to_str(gyro_z));Serial.print(","); 
+  Serial.print(convert_int16_to_str(gyro_y)); Serial.print(","); 
+  Serial.print(convert_int16_to_str(gyro_x));
+  Serial.println();
+  
+  delay(20);
 }
